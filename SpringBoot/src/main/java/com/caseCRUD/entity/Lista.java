40608796abc96235bc1d861170a41e0659a88748
Felipe Medeiros <67637742+FelipeMDR@users.noreply.github.com>
2023-12-14 04:32:32 -0300

    Alterando nome da tabela

--- SpringBoot/src/main/java/com/caseCRUD/entity/Lista.java
+++ SpringBoot/src/main/java/com/caseCRUD/entity/Lista.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 @DataAmount
 @AllArgsConstructor
 @NoArgsConstructor
-@DynamoDBTable(tableName = "Casecrud")
+@DynamoDBTable(tableName = "tdl-db")
 public class Lista {
 	
     private String id;
